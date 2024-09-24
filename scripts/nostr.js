21dd6bccade0b6a6c63d4bbd313354438e79974e
tohrxyz <tomas.hrib@proton.me>
2024-09-24 21:22:34 +0200

    allow changing npub and clear everything page

--- scripts/nostr.js
+++ scripts/nostr.js
@@ -1,18 +1,48 @@
 const relayUrl = "wss://relay.damus.io"
-const npub = "npub19u6u0h9pwg8k63e9f535enqdkgzl8wqzwzlnrtnwr853wt4ypszsapm6e6"
 let relay;
 let events = [];
+let nostr;
+let notesContainer;
 
 const newNoteEvent = new CustomEvent("newNoteEvent", { detail: { message: "New event!!!" }})
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const nostr = window.NostrTools
+  nostr = window.NostrTools
   console.log({nostr})
   
   relay = await nostr.Relay.connect(relayUrl)
   console.log(`Connected to ${relay.url}`, {relay})
-  const hexPubKey = nostr.nip19.decode(npub).data
 
+  const btn = document.getElementById("npub-submit-input")
+  btn.addEventListener('click', submitNpub)
+})
+
+window.addEventListener('newNoteEvent', (event) => {
+  console.log(event.detail.message)
+  notesContainer = document.getElementById('nostr-notes-container')
+    const text = document.createElement('p');
+    const latestNote = events.pop()
+    if (latestNote.tags[1][3] === "reply") {
+      text.textContent = "RE: " + latestNote.content
+    } else {
+      text.textContent = latestNote.content
+    }
+    // text.textContent = events.pop().content
+    notesContainer.appendChild(text)
+    notesContainer.children.l
+})
+
+function submitNpub() {
+  console.log("clicked on submit npub")
+  events = []
+  if (notesContainer) {
+    while(notesContainer.firstChild) {
+      notesContainer.removeChild(notesContainer.lastChild)
+    }
+  }
+  const input = document.getElementById("npub-input")
+  const npub = input.value
+  const hexPubKey = nostr.nip19.decode(npub).data
   relay.subscribe([
     {
       kinds: [1],
@@ -26,18 +56,4 @@ document.addEventListener('DOMContentLoaded', async () => {
       window.dispatchEvent(newNoteEvent)
     }
   })
-})
-
-window.addEventListener('newNoteEvent', (event) => {
-  console.log(event.detail.message)
-  const notesContainer = document.getElementById('nostr-notes-container')
-    const text = document.createElement('p');
-    const latestNote = events.pop()
-    if (latestNote.tags[1][3] === "reply") {
-      text.textContent = "RE: " + latestNote.content
-    } else {
-      text.textContent = latestNote.content
-    }
-    // text.textContent = events.pop().content
-    notesContainer.appendChild(text)
-})
+}
